50bb6bee73ab34240e2ad4d69036b2692b625e9e
FireGhost1301 <88044725+FireGhost1301@users.noreply.github.com>
2022-11-08 23:14:58 +0700

    Update PBDS.h

--- content/data-structures/PBDS.h
+++ content/data-structures/PBDS.h
@@ -3,17 +3,18 @@
  * Date: 2022-11-08
  * License: CC0
  * Source: Codeforces
- * Description: \texttt{find_by_order(int k)}: return an iterator
- * to the k-th smallest element, counting from 0.
- * \texttt{order_of_key(int k)}: return the number of
- * items strictly smaller than k.
+ * Description: in code
  * Time: O(\log N)
  */
 
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-
+/**
+    Support find_by_order(int k): return an iterator to the k-th smallest element,
+                                  counting from 0
+            order_of_key(int k) : return the number of items strictly smaller than k
+*/
 template<typename T> using ordered_set =
 tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 template<typename Key, typename T> using ordered_map =
