fae05e9ee66fab475cf9b79e55b4d00f9f899be8
FireGhost1301 <88044725+FireGhost1301@users.noreply.github.com>
2022-11-08 22:47:13 +0700

    Update PBDS.h

--- content/data-structures/PBDS.h
+++ content/data-structures/PBDS.h
@@ -1,12 +1,19 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
 /**
-    Policy-based data structures
+ * Author: FireGhost
+ * Date: 2022-08-11
+ * License: CC0
+ * Source: Folklore
+ * Description: Policy-based data structures
     Support find_by_order(int k): return an iterator to the k-th smallest element,
                                   counting from 0
             order_of_key(int k) : return the number of items strictly smaller than k
+ * Status: stress-tested
 */
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
 template<typename T> using ordered_set =
 tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 template<typename Key, typename T> using ordered_map =
